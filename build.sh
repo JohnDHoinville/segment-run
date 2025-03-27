0641bd288fa958147fd37f6bb1a2e05f5e050264
JohnDHoinville <JohnDHoinville@gmail.com>
2025-03-27 00:28:19 -0400

    Copy static files to correct locations for Heroku

--- build.sh
+++ build.sh
@@ -8,13 +8,17 @@ npm run build
 echo "Creating necessary directories..."
 mkdir -p backend/static
 mkdir -p backend/templates
+mkdir -p static
+mkdir -p templates
 
 # Copy static files from React build to backend/static
 echo "Copying static files..."
 cp -r build/static/* backend/static/
+cp -r build/static/* static/
 
 # Copy index.html to backend/templates
 echo "Copying index.html..."
 cp build/index.html backend/templates/
+cp build/index.html templates/
 
 echo "Build complete!" 
